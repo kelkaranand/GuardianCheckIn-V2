ac9aa5d01c4f091ad47736b0007a4724745aeba2
anandkelkar <kelkaranand28@gmail.com>
2020-01-10 12:43:34 -0500

    Anand - Nothing to see here

--- Guardian Check-in/SearchStudent/SearchStudentViewController.swift
+++ Guardian Check-in/SearchStudent/SearchStudentViewController.swift
@@ -25,9 +25,9 @@ class SearchStudentViewController: UIViewController {
     @IBOutlet weak var checkInLabel: UILabel!
     @IBOutlet weak var checkInButtonView: UIView!
     @IBOutlet weak var superView: UIView!
+    @IBOutlet weak var logoImage: UIImageView!
     var moved = false
-    
-    @IBOutlet var centerConstraint: NSLayoutConstraint!
+    var easterEgg = false
     
     
     override func viewDidLayoutSubviews() {
@@ -74,6 +74,7 @@ class SearchStudentViewController: UIViewController {
                 self.superView.center.y = self.superView.center.y - self.view.bounds.height
             }
         }
+        self.mainCard.isHidden = true
     }
     
     override func viewDidLoad() {
@@ -90,21 +91,44 @@ class SearchStudentViewController: UIViewController {
         //Setup starting position for card
         mainCard.center.y = mainCard.center.y + self.view.bounds.height
         
-        //Rotation gesture to show setup
-//        let rotateGesture = UIRotationGestureRecognizer(target: self, action: #selector(showSetup(_:)))
-//        self.view.addGestureRecognizer(rotateGesture)
-        
         //Tap gesture for check-in button
-        checkInCard.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(checkInPressed)))
+        checkInButtonView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(checkInPressed)))
         
         //Swipe right to flip back card
         let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(flipBack))
         rightSwipe.direction = .right
         mainCard.addGestureRecognizer(rightSwipe)
+        
+        //Easter egg gesture recognizer
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(showLebron))
+        longPress.minimumPressDuration = 5
+        logoImage.addGestureRecognizer(longPress)
     
     }
     
+    @objc func showLebron() {
+        if(!easterEgg)
+        {
+            easterEgg = true
+            UIView.transition(with: logoImage, duration: 0.5, options: [.transitionFlipFromLeft, .showHideTransitionViews], animations: {
+                self.logoImage.alpha = 0
+            }, completion: { finished in
+                self.logoImage.image = UIImage.gifImageWithURL("https://media0.giphy.com/media/Y506ebOM2Zd4llHv43/source.gif")
+                self.logoImage.alpha = 1
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                    UIView.animate(withDuration: 0.5, animations: {
+                        self.logoImage.alpha = 0
+                    }, completion: { finished in
+                        self.logoImage.image = UIImage.init(named: "LJFF_logo")
+                        self.logoImage.alpha = 1
+                    })
+                }
+            })
+        }
+    }
+    
     @objc func checkInPressed() {
+        self.mainCard.isHidden = false
         UIView.transition(with: checkInCard, duration: 0.5, options: [.transitionFlipFromRight, .showHideTransitionViews], animations: {
             self.checkInCard.alpha = 0
         }, completion: { finished in
@@ -127,19 +151,10 @@ class SearchStudentViewController: UIViewController {
         UIView.transition(with: checkInCard, duration: 0.5, options: [.transitionFlipFromLeft, .showHideTransitionViews], animations: {
             self.checkInCard.alpha = 1
         }, completion: { finished in
-            
+            self.mainCard.isHidden = true
         })
         print("flip")
     }
- 
-//    @objc func showSetup(_ sender: UIRotationGestureRecognizer) {
-//        if rad2deg(Double(sender.rotation)) > 120 {
-//            if !moved {
-//                moved = true
-//                moveToSetup()
-//            }
-//        }
-//    }
     
     func moveToSetup(){
         UIView.animate(withDuration: 0.5, animations: {
@@ -159,7 +174,7 @@ extension SearchStudentViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         if let cell = tableView.dequeueReusableCell(withIdentifier: "result", for: indexPath) as? SearchStudentResultCell {
-//                cell.backgroundColor = .blue
+
             let fragFname = filteredStudentrecords[indexPath.row].fname
             let fragLname = filteredStudentrecords[indexPath.row].lname
             cell.nameLabel.text = fragFname + " " + fragLname
@@ -246,5 +261,176 @@ extension SearchStudentViewController: UITableViewDelegate {
                 self.performSegue(withIdentifier: "showEnd", sender: self)
             }
         })
+    }
+}
+
+//Do not touch anything below this line!!!
+
+fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+    switch (lhs, rhs) {
+    case let (l?, r?):
+        return l < r
+    case (nil, _?):
+        return true
+    default:
+        return false
+    }
+}
+
+extension UIImage {
+    
+    public class func gifImageWithData(_ data: Data) -> UIImage? {
+        guard let source = CGImageSourceCreateWithData(data as CFData, nil) else {
+            print("image doesn't exist")
+            return nil
+        }
+        
+        return UIImage.animatedImageWithSource(source)
+    }
+    
+    public class func gifImageWithURL(_ gifUrl:String) -> UIImage? {
+        guard let bundleURL:URL = URL(string: gifUrl)
+            else {
+                print("image named \"\(gifUrl)\" doesn't exist")
+                return nil
+        }
+        guard let imageData = try? Data(contentsOf: bundleURL) else {
+            print("image named \"\(gifUrl)\" into NSData")
+            return nil
+        }
+        
+        return gifImageWithData(imageData)
+    }
+    
+    public class func gifImageWithName(_ name: String) -> UIImage? {
+        guard let bundleURL = Bundle.main
+            .url(forResource: name, withExtension: "gif") else {
+                print("SwiftGif: This image named \"\(name)\" does not exist")
+                return nil
+        }
+        guard let imageData = try? Data(contentsOf: bundleURL) else {
+            print("SwiftGif: Cannot turn image named \"\(name)\" into NSData")
+            return nil
+        }
+        
+        return gifImageWithData(imageData)
+    }
+    
+    class func delayForImageAtIndex(_ index: Int, source: CGImageSource!) -> Double {
+//        var delay = 0.05
+        
+        let cfProperties = CGImageSourceCopyPropertiesAtIndex(source, index, nil)
+        let gifProperties: CFDictionary = unsafeBitCast(
+            CFDictionaryGetValue(cfProperties,
+                                 Unmanaged.passUnretained(kCGImagePropertyGIFDictionary).toOpaque()),
+            to: CFDictionary.self)
+        
+        var delayObject: AnyObject = unsafeBitCast(
+            CFDictionaryGetValue(gifProperties,
+                                 Unmanaged.passUnretained(kCGImagePropertyGIFUnclampedDelayTime).toOpaque()),
+            to: AnyObject.self)
+        if delayObject.doubleValue == 0 {
+            delayObject = unsafeBitCast(CFDictionaryGetValue(gifProperties,
+                                                             Unmanaged.passUnretained(kCGImagePropertyGIFDelayTime).toOpaque()), to: AnyObject.self)
+        }
+        
+        var delay = delayObject as! Double
+        
+//        if delay < 0.1 {
+//            delay = 0.1
+//        }
+        
+        return delay
+    }
+    
+    class func gcdForPair(_ a: Int?, _ b: Int?) -> Int {
+        var a = a
+        var b = b
+        if b == nil || a == nil {
+            if b != nil {
+                return b!
+            } else if a != nil {
+                return a!
+            } else {
+                return 0
+            }
+        }
+        
+        if a < b {
+            let c = a
+            a = b
+            b = c
+        }
+        
+        var rest: Int
+        while true {
+            rest = a! % b!
+            
+            if rest == 0 {
+                return b!
+            } else {
+                a = b
+                b = rest
+            }
+        }
+    }
+    
+    class func gcdForArray(_ array: Array<Int>) -> Int {
+        if array.isEmpty {
+            return 1
+        }
+        
+        var gcd = array[0]
+        
+        for val in array {
+            gcd = UIImage.gcdForPair(val, gcd)
+        }
+        
+        return gcd
+    }
+    
+    class func animatedImageWithSource(_ source: CGImageSource) -> UIImage? {
+        let count = CGImageSourceGetCount(source)
+        var images = [CGImage]()
+        var delays = [Int]()
+        
+        for i in 0..<count {
+            if let image = CGImageSourceCreateImageAtIndex(source, i, nil) {
+                images.append(image)
+            }
+            
+            let delaySeconds = UIImage.delayForImageAtIndex(Int(i),
+                                                            source: source)
+            delays.append(Int(delaySeconds * 1000.0)) // Seconds to ms
+        }
+        
+        let duration: Int = {
+            var sum = 0
+            
+            for val: Int in delays {
+                sum += val
+            }
+            
+            return sum
+        }()
+        
+        let gcd = gcdForArray(delays)
+        var frames = [UIImage]()
+        
+        var frame: UIImage
+        var frameCount: Int
+        for i in 0..<count {
+            frame = UIImage(cgImage: images[Int(i)])
+            frameCount = Int(delays[Int(i)] / gcd)
+            
+            for _ in 0..<frameCount {
+                frames.append(frame)
+            }
+        }
+        
+        let animation = UIImage.animatedImage(with: frames,
+                                              duration: Double(duration) / 1000.0)
+        
+        return animation
     }
 }
